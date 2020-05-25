0dd494a0ba9e50808a0c07248740ff0b3104db45
tidesec <tidesecplus@gmail.com>
2020-05-25 11:27:25 +0800

    Update

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 nohup /usr/local/mongodb/bin/mongod --dbpath=/data/db --auth -bind_ip 0.0.0.0 &
 cd /root/Mars && nohup python mars.py &
 nohup su -l acunetix -c /home/acunetix/.acunetix_trial/start.sh &
-cd /root/Mars/taskpython/ && nohup python asset_task_scan_v1.0.py &
+cd /root/Mars/taskpython/ && nohup python asset_task_scan_v1.0.py & 
